ba4877bcae055f2f753f5f62e1c27d029c6c910f
Isaac Santiago <eoisaacc@gmail.com>
2023-11-05 13:00:34 -0300

    feat: create bottom navigation menu

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.eoisaac.wod"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.eoisaac.wod"
--- app/src/main/java/com/eoisaac/wod/MainActivity.kt
+++ app/src/main/java/com/eoisaac/wod/MainActivity.kt
@@ -2,10 +2,33 @@ package com.eoisaac.wod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.eoisaac.wod.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaplaceFragment(WorkoutFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_workout -> replaplaceFragment(WorkoutFragment())
+                R.id.navigation_exercises -> replaplaceFragment(ExercisesFragment())
+                R.id.navigation_profile -> replaplaceFragment(ProfileFragment())
+//                else -> {}
+            }
+            true
+        }
+    }
+
+    private fun replaplaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_activity_frame_layout, fragment)
+        transaction.commit()
     }
 }
