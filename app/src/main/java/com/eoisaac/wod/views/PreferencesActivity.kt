3a6374a73a8f618f450cfac57c20ce2fc878fcbb
Isaac Santiago <eoisaacc@gmail.com>
2023-11-08 14:05:56 -0300

    feat: add profile activities buttons

---
+++ app/src/main/java/com/eoisaac/wod/views/PreferencesActivity.kt
@@ -0,0 +1,12 @@
+package com.eoisaac.wod.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eoisaac.wod.R
+
+class PreferencesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preferences)
+    }
+}
