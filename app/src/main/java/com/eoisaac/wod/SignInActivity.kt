66242736f364653434508d17516a803d557cf0d8
Isaac Santiago <eoisaacc@gmail.com>
2023-11-07 21:21:29 -0300

    feat: navigate by intent between sign in and sign up activities

--- app/src/main/java/com/eoisaac/wod/SignInActivity.kt
+++ app/src/main/java/com/eoisaac/wod/SignInActivity.kt
@@ -1,10 +1,12 @@
 package com.eoisaac.wod
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.eoisaac.wod.databinding.ActivitySignInBinding
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivitySignInBinding
 
@@ -12,5 +14,23 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupOnClickListeners()
+    }
+
+
+    private fun setupOnClickListeners() {
+        binding.navigateToSignUpButton.setOnClickListener(this)
+    }
+
+    override fun onClick(button: View) {
+        when (button.id) {
+            binding.navigateToSignUpButton.id -> navigateToSignUpActivity()
+        }
+    }
+
+    private fun navigateToSignUpActivity() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/eoisaac/wod/SignUpActivity.kt
+++ app/src/main/java/com/eoisaac/wod/SignUpActivity.kt
@@ -1,11 +1,34 @@
 package com.eoisaac.wod
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.eoisaac.wod.databinding.ActivitySignUpBinding
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupOnClickListeners()
+    }
+
+    private fun setupOnClickListeners() {
+        binding.navigateToSignInButton.setOnClickListener(this)
+    }
+
+    override fun onClick(button: View) {
+        when (button.id) {
+            binding.navigateToSignInButton.id -> navigateToSignInActivity()
+        }
+    }
+
+    private fun navigateToSignInActivity() {
+        val intent = Intent(this, SignInActivity::class.java)
+        startActivity(intent)
     }
 }
