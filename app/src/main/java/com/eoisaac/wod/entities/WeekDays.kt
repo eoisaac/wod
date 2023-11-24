2c2a5b138b70f665c14649c62ad6a4723403fcd7
Isaac Santiago <eoisaacc@gmail.com>
2023-11-24 15:06:37 -0300

    feat: create snackbar styles

---
+++ app/src/main/java/com/eoisaac/wod/entities/WeekDays.kt
@@ -0,0 +1,11 @@
+package com.eoisaac.wod.entities
+
+enum class WeekDays(val day: String, val index: Int) {
+    MONDAY("MONDAY", 0),
+    TUESDAY("TUESDAY", 1),
+    WEDNESDAY("WEDNESDAY", 2),
+    THURSDAY("THURSDAY", 3),
+    FRIDAY("FRIDAY", 4),
+    SATURDAY("SATURDAY", 5),
+    SUNDAY("SUNDAY ", 6)
+}
--- app/src/main/java/com/eoisaac/wod/views/NewWorkoutFragment.kt
+++ app/src/main/java/com/eoisaac/wod/views/NewWorkoutFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import com.eoisaac.wod.databinding.FragmentNewWorkoutBinding
 import com.eoisaac.wod.viewModels.NewWorkoutViewModel
+import com.google.android.material.snackbar.Snackbar
 
 
 class NewWorkoutFragment : Fragment(), View.OnClickListener {
@@ -36,6 +38,29 @@ class NewWorkoutFragment : Fragment(), View.OnClickListener {
     }
 
     private fun handleCreateNewWorkout() {
+        with(binding) {
+            if (nameTextInputField.text.isNullOrEmpty()) {
+                nameTextInputField.error = "Name cannot be empty"
+                Snackbar.make(binding.root, "Name cannot be empty", Snackbar.LENGTH_SHORT)
+                    .setAction("OK") { nameTextInputField.requestFocus() }
+                    .show()
+                return
+            }
+
+            val checkboxes = listOf(
+                mondaysCheckbox, tuesdaysCheckbox, wednesdaysCheckbox, thursdaysCheckbox,
+                fridaysCheckbox, saturdaysCheckbox, sundaysCheckbox
+            )
+
+            val checkboxStatuses = mutableListOf<Boolean>()
+            checkboxes.forEach { checkbox -> checkboxStatuses.add(checkbox.isChecked) }
+
+            if (checkboxStatuses.all { !it }) {
+                Toast.makeText(requireContext(), "Please select at least one day", Toast.LENGTH_SHORT).show()
+                return
+            }
+        }
+
         val name = binding.nameTextInputField.text.toString()
         val weekDay = 0
 
